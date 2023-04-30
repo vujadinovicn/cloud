c80d63287828d5ee0bd786e7a4dbca52af0b561d
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-04-30 20:40:05 +0200

    added routing for register component

--- front/cloud-drive/src/infrastructure/app-routing.module.ts
+++ front/cloud-drive/src/infrastructure/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from 'src/app/sign-up/sign-up.component';
 import { SignInComponent } from '../app/sign-in/sign-in.component';
 
-const routes: Routes = [ ];
+const routes: Routes = [ 
+  {path:'login', component: SignInComponent},
+  {path: 'register', component: SignUpComponent},
+  {path: '**', component: SignInComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
