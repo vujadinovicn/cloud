7a33d0ee01148276173ea5842a518cc4fcf69c00
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-05-04 00:44:44 +0200

    implemented ts logic in file-upload component

--- front/cloud-drive/src/app/file-upload/file-upload.component.ts
+++ front/cloud-drive/src/app/file-upload/file-upload.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -9,6 +10,17 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent implements OnInit {
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required,]),
+    description: new FormControl('', [Validators.required,]),
+    tag: new FormControl('', [Validators.required,]),
+  }, [])
+
+  tags: String[] = []
+  size: String = '0kb'
+
+  right_part_visible: boolean = false;
 
   constructor(private http: HttpClient) { }
 
@@ -26,6 +38,9 @@ export class FileUploadComponent implements OnInit {
       reader.onload=(e: any)=>{
         this.profileImgPath = reader.result as string;
       }
+      this.form.controls['name'].setValue(this.file.name);
+      this.size = Math.round(this.file.size/1024).toString() + 'kB';
+      this.right_part_visible = true;
     }
   }
 
@@ -52,15 +67,30 @@ export class FileUploadComponent implements OnInit {
     };
     let o = {
       id: "ss",
-      name: this.file.name,
+      name: this.form.value.name,
       lastModified:  new Date().toISOString().split('T')[0],
       type: this.file.type, 
       size: this.file.size,
       createdAt: new Date().toISOString().split('T')[0],
-      description: "aaabbababa",
-      tags: ["a", "b", "c"]
+      description: this.form.value.description,
+      tags: this.tags
     }
     return this.http.post<any>(environment.apiGateway + '/metadata', o, options);
+  }
+
+
+
+
+  remove_tag(name: String): void {
+    this.tags = this.tags.filter(item => item != name);
+  }
+
+  add_tag(): void {
+    if (this.form.value.tag) {
+      if (!this.tags.includes(this.form.value.tag)) {
+        this.tags.push(this.form.value.tag)
+      }  
+    }
   }
 
 }
