8679d0e367093add30883c7c0b8f9e6d0bc905f1
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-06-24 20:19:24 +0200

    implemented lambda function for getting cognito users by username

---
+++ cloud-drive/get-user-by-username/get-user-by-username.py
@@ -0,0 +1,42 @@
+import boto3
+import json
+import base64
+import os
+from utility.utils import create_response
+
+s3 = boto3.client('s3')
+bucket_name = os.environ['BUCKET_NAME']
+
+
+def handler(event, context):
+    
+    
+    try:
+        username = event['queryStringParameters']['username']
+        idp = boto3.client('cognito-idp')
+        response = idp.list_users(
+            UserPoolId='eu-central-1_wnNKMwD5v',
+            Filter='username = "{}"'.format(username)
+        )
+
+        if response['Users']:
+            user = response['Users'][0]
+            user_attributes = user['Attributes']
+        
+        # Create a dictionary to store the user attributes
+            user_data = {}
+        
+        # Extract and store the user attributes in the dictionary
+            for attribute in user_attributes:
+                user_data[attribute['Name']] = attribute['Value']
+            # Return the user attributes
+            return create_response(200, user_data)
+        else:
+            return create_response(404, "User not found")
+    
+    except s3.exceptions.UserNotFoundException:
+        return create_response(404, "User not found")
+    
+    except Exception as e:
+        return create_response(404, "Error happened")
+    
