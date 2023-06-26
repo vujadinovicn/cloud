2d913cc965b215c47e54974b4b04defb4b8ceab0
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-06-26 21:06:05 +0200

    refactored create-folder lambda to write new folder into dynamo as well

--- cloud-drive/create-folder/create-folder.py
+++ cloud-drive/create-folder/create-folder.py
@@ -7,11 +7,20 @@ from utility.utils import create_response
 s3 = boto3.client('s3')
 bucket_name = os.environ['BUCKET_NAME']
 
+table_name = os.environ['FOLDER_TABLE_NAME']
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(table_name)
+
 def handler(event, context):
     try:
         folder_name = event['queryStringParameters']['foldername']
+        data = json.loads(event['body'])
 
         username = event['requestContext']['authorizer']['claims']['cognito:username']
+        if (not data['id'].startswith(username)):
+            data["id"] = username + "/" + data["id"]
+
+        table.put_item(Item=data)
 
         folder_key = f"{folder_name}/"
         s3.put_object(Bucket=bucket_name, Key=username + "/" + folder_key)
