d43f2eeeaa5b8bc0d1acf3d15f06ba0099d31202
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-06-27 17:25:06 +0200

    finished transactional file deletion

--- cloud-drive/delete-file/delete-file.py
+++ cloud-drive/delete-file/delete-file.py
@@ -13,20 +13,40 @@ table = dynamodb.Table(table_name)
 def handler(event, context):
     try:
         username = event['requestContext']['authorizer']['claims']['cognito:username']
-        path = username + '/' + event['queryStringParameters']['filename']
+        path = event['queryStringParameters']['filename']
+        if (not path.startswith(username)):
+            path = username + '/' + username
 
-        s3_client = boto3.client('s3')
+        response = table.get_item(Key={'id': path})
+        old_item = response['Item']
 
-        with table.batch_writer() as batch:
-            response = batch.delete_item(Key={
-                    "id": path
-                })
-
-            
-        s3_client.delete_object(Bucket=bucket_name, Key=path)
-
-        return create_response(200, "File '{path}' is deleted successfully.")
+        if (delete_file_from_dynamo(path)):
+            if (delete_file_from_s3(path)):
+                return create_response(200, "File '{path}' is deleted successfully.")
+            else:
+                table.put_item(Item=old_item)                    
+                return create_response(500, "Failed to delete file from s3.")
+        else:
+            return create_response(500, "Failed to delete file from Dyanmodb.")
     
     except Exception as e:
         return create_response(500, str(e))
     
+
+def delete_file_from_dynamo(path):
+    try:
+        with table.batch_writer() as batch:
+            response = batch.delete_item(Key={
+                    "id": path
+                })        
+        return True
+    except Exception as e:
+        return False
+    
+
+def delete_file_from_s3(path):
+    try:
+        s3.delete_object(Bucket=bucket_name, Key=path)
+        return True
+    except Exception as e:
+        return False
